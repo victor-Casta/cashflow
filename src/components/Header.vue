04e81a4178da021e662fbb209b8946388932cd7e
victorarroyavecastaneda <victorarroyavecastaneda@gmail.com>
2024-05-18 23:00:38 +0000

    Header y contenido

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <img @click="reload" src="../assets/cash-flow-icon.svg" alt="logo">
+        <h1>Cash<span>Flow</span></h1>
+        <span></span>
+    </header>
+</template>
+
+<script>
+    export default {
+        methods: {
+            reload() {
+                window.location.reload();
+            }
+        }
+    }
+</script>
+
+<style>
+    header {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        width: 100vw;
+    }
+
+    img,
+    span {
+        width: 48px;
+    }
+
+    h1 {
+        font-size: 1.4rem;
+        color: #529fdf;
+    }
+
+    h1 span {
+        color: #76e7b8;
+    }
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,19 @@
 <template>
-    <p>Hello world</p>
+    <Layout>
+        <template #header>
+            <Header />
+        </template>
+    </Layout>
 </template>
+
+<script>
+    import Layout from './Layout.vue';
+    import Header from './Header.vue';
+
+    export default {
+        components: {
+            Layout,
+            Header
+        }
+    }
+</script>
