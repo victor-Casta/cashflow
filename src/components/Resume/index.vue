5f165796e40392b3723549a9268523acbd79c1bc
victorarroyavecastaneda <victorarroyavecastaneda@gmail.com>
2024-05-21 15:49:37 +0000

    Formato de moneda

--- src/components/Resume/index.vue
+++ src/components/Resume/index.vue
@@ -1,4 +1,7 @@
 <script>
+const currencyFormater = new Intl.NumberFormat(("es-CO"), {
+  style: "currency", currency: "COP"
+})
 export default {
     props: {
         totalLabel: {
@@ -22,6 +25,9 @@ export default {
         },
         amountVisual() {
             return this.amount !== null ? this.amount : this.totalAmount;
+        },
+        amountCurrency() {
+            return currencyFormater.format(this.amountVisual)
         }
     },
 };
@@ -30,7 +36,13 @@ export default {
 <template>
     <main>
         <p>{{ labelVisual }}</p>
-        <h1>{{ amountVisual }}</h1>
+        <h1>{{ amountCurrency }}</h1>
+        <div class="graphic">
+            <slot name="graphic"></slot>
+        </div>
+        <div class="action">
+            <slot name="action"></slot>
+        </div>
     </main>
 </template>
 
